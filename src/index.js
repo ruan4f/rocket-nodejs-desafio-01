8a50c4447583c45ea44af60a4061b13bee8fcb34
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-09 13:40:41 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(400).json({
+      error: "User not found",
+    });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
