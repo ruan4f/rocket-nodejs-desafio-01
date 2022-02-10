842dcc91f21c020468e232a63bd1ba8de96950bc
Ruan Ferreira <8647151+ruan4f@users.noreply.github.com>
2022-02-09 23:11:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-
 const { v4: uuidv4 } = require("uuid");
 
 const app = express();
@@ -11,7 +10,6 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
   const { username } = request.headers;
   const user = users.find((user) => user.username === username);
 
@@ -27,12 +25,9 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 app.post("/users", (request, response) => {
-  // Complete aqui
   const { name, username } = request.body;
 
-  const userAlreadyExist = users.some(
-    (user) => user.username === username
-  );
+  const userAlreadyExist = users.some((user) => user.username === username);
 
   if (userAlreadyExist) {
     return response.status(400).json({
@@ -45,7 +40,7 @@ app.post("/users", (request, response) => {
     name,
     username,
     todos: [],
-  }
+  };
 
   users.push(user);
 
@@ -53,23 +48,56 @@ app.post("/users", (request, response) => {
 });
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post("/todos", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
 });
 
 app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+  user.todos = user.todos.map((todo) =>
+    todo.id === id
+      ? {
+          ...todo,
+          title,
+          deadline,
+        }
+      : todo
+  );
+
+  return response.status(201).send();
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.query;
+
+  user.todos = user.todos.map((todo) =>
+    todo.id === id
+      ? {
+          ...todo,
+          done: !todo.done,
+        }
+      : todo
+  );
+
+  return response.status(201).send();
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.query;
+  const todo = user.todos.filter((todo) => todo.id === id);
+
+  user.todos.splice(todo, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
